095ecfb6a9141ddc9820fd26aa694d783504b1cd
R.M.Jagerman@student.tudelft.nl <R.M.Jagerman@student.tudelft.nl>
2013-04-25 12:38:28 +0200

    Updated tests in monument bundle

--- src/Monubit/MonumentBundle/Tests/Controller/MonumentControllerTest.php
+++ src/Monubit/MonumentBundle/Tests/Controller/MonumentControllerTest.php
@@ -6,12 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MonumentControllerTest extends WebTestCase
 {
-    public function testMonument()
+    public function testMonumentNotFound()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/monument/2');
-
-        $this->assertTrue($crawler->filter('html:contains("Monument")')->count() > 0);
     }
 }
