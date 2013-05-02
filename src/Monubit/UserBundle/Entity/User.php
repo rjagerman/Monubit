da809717d699fe47a086e8b86ac5fc0e3969c528
R.M.Jagerman@student.tudelft.nl <R.M.Jagerman@student.tudelft.nl>
2013-05-02 10:22:56 +0200

    Cleaned up code for user entity

--- src/Monubit/UserBundle/Entity/User.php
+++ src/Monubit/UserBundle/Entity/User.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Monubit\UserBundle\Entity;
-
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -9,18 +8,17 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="monubit_user")
  */
-class User extends BaseUser
-{
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
+class User extends BaseUser {
+	
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
 
-    public function __construct()
-    {
-        parent::__construct();
-        
-    }
+	public function __construct() {
+		parent::__construct();
+	}
+
 }
