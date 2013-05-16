7138781a48b12b0ea3936c51795917eef4361f3a
Rolf Jagerman <rjagerman@gmail.com>
2013-05-16 09:05:36 +0200

    Added Liip Imagine Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
 			new Monubit\UserBundle\MonubitUserBundle(),
             new Monubit\RatingsBundle\MonubitRatingsBundle(),
 			new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
+        	new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
