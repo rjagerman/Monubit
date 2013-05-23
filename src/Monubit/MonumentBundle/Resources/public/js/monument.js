6188c91a5ef60942209524b00099940df59674e2
U-ToshibaC660\Zakaria <Zakaria@ToshibaC660.(none)>
2013-05-23 11:00:44 +0200

    Improved tag functionality

--- src/Monubit/MonumentBundle/Resources/public/js/monument.js
+++ src/Monubit/MonumentBundle/Resources/public/js/monument.js
@@ -1,35 +1,39 @@
+function addTag(me) {
+   	console.log("Voeg tag toe");
+   	var url = $('#addtag').attr('href').replace("__tag__", $("#tagname").val());
+   	console.log(url);
+   	$.get(url, function(json) {
+	   	if(json[0] == "success"){
+	   		$('.tags').replaceWith(json.html);
+	   	} else{
+	   		alert(json.error.message);
+	   		$("#tag-dialog").dialog( "open" );
+	   	}
+   	});
+   	$(me).dialog( "close" );
+}
+
 $(function() {
+	$("#addtag").click(function() {
+		$("#tag-dialog").dialog( "open" );
+		return false;
+	});
+	
 	$("#tag-dialog").dialog({
 	    autoOpen: false,
 	    height: 300,
 	    width: 350,
 	    modal: true,
 	    buttons: {
-		   	"Voeg de tag toe!": function() {
-			   	console.log("Voeg tag toe");
-			   	var url = $('#addtag').attr('href').replace("__tag__", $("#tagname").val());
-			   	console.log(url);
-			   	$.get(url, function(json) {
-				   	if(json[0] == "success"){
-				   		$('.tags').replaceWith(json.html);
-				   	} else{
-				   		alert(json.error.message);
-				   		$("#tag-dialog").dialog( "open" );
-				   	}
-			   	});
-			   	$('#tag-dialog').live('keyup', function(e){
-			   	  if (e.keyCode == 13) {
-			   	    $(':button:contains("OK")').click();
-			   	  }
-			   	});
-			   	$( this ).dialog( "close" );
-		   	}
+		   	"Voeg de tag toe!": function() { addTag(this); }
+	    },
+	    open: function() {
+	    	$("#tag-dialog").keypress(function(e) {
+			    if (e.keyCode == $.ui.keyCode.ENTER) {
+			    	addTag(this);
+			    	return false;
+			    }
+			});
 	    }
-	});
-	 
-	$("#addtag").click(function() {
-		$("#tag-dialog").dialog( "open" );
-		return false;
-	});
-	$('#addtag').button();
+	});	
 });
