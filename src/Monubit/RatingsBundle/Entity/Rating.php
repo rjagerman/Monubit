e1d5ed9abce83b613935f7c819a5f44a20d979bc
Rolf Jagerman <rjagerman@gmail.com>
2013-05-28 23:31:05 +0200

    Added indexes and uniqueness constraints
    Added indexes to speed up calculating averages of ratings and added
    uniqueness constraints to ensure only a single rating per monument per
    user.

--- src/Monubit/RatingsBundle/Entity/Rating.php
+++ src/Monubit/RatingsBundle/Entity/Rating.php
@@ -2,12 +2,23 @@
 namespace Monubit\RatingsBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 /**
  * A Rating
+ * 
  *
  * @ORM\Entity
- * @ORM\Table(name="rating")
+ * @ORM\Table(name="rating",
+ * 	uniqueConstraints={
+ * 		@ORM\UniqueConstraint(name="monumentuser_unique", columns={"monument_id", "user_id"})
+ * 	},
+ * 	indexes={
+ * 		@ORM\index(name="monumentrating_idx", columns={"monument_id", "rating"})
+ * 	}
+ * )
+ * @UniqueEntity({"monument", "user"})
  */
 class Rating {
 	
