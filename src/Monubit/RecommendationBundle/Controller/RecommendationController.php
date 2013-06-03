f2cadb3e7a4036bbf461b39dc16d185c3e907bbc
U-ToshibaC660\Zakaria <Zakaria@ToshibaC660.(none)>
2013-06-03 10:11:11 +0200

    modified recommendation system

---
+++ src/Monubit/RecommendationBundle/Controller/RecommendationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Monubit\RecommendationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class RecommendationController extends Controller {
+	/**
+	 * @Route("/recommendation/{id}", requirements={"id" = "\d+"})
+	 * @Template()
+	 */
+    public function recommendationAction($id) {
+    	// Initialize settings
+    	$repository = $this->getDoctrine()->getManager()
+    		->getRepository('MonubitMonumentBundle:Monument');
+		
+    	// Get the monument
+    	$monument = $repository->find($id);
+    	
+    	$tags = $monument->getTags();
+    	$recommended = array();
+    	
+    	for($i = 0; $i < min(4, count($tags));$i++) {
+    		$monuments = $tags[$i]->getMonuments();
+    		if($monuments[$i]->getId() != $id) {
+    			$recommended[$i] = $monuments[$i];
+    		}
+    	}
+    	return array('recommended' => $recommended);
+    }
+}
