447def147ad14007e7ed3ad48e5f653bacceb364
Berend <berendottervanger@gmail.com>
2013-06-14 12:30:54 +0200

    added some functionality to maps

--- src/Monubit/MapBundle/Resources/public/js/maps.js
+++ src/Monubit/MapBundle/Resources/public/js/maps.js
@@ -3,6 +3,14 @@ var clusterer;
 var markers = [];
 
 function initialize() {
+	var url = window.location.href;
+	var id = -1;
+	if(url.indexOf("#") != -1) {
+		id = url.substring(url.indexOf("#")+1);
+	}
+	else {
+		
+	}
 	var nederland = new google.maps.LatLng(52.0833, 5.1333);
 	var mapOptions = {
 			zoom: 7,
@@ -14,13 +22,23 @@ function initialize() {
 
 	
 	$.ajax({
-		url: "/maps/monument",
+		url: getMonumentsURL,
 		type: "POST"
 	}).done(
 			function(json) {
 				if(json[0] == "success"){
+					for(var i in json.data) {
+						if(json.data[i].id == id) {
+							console.log(json.data[i].id);
+							console.log(json.data[i].latitude);
+							console.log(json.data[i].longitude);
+							map.setCenter(new google.maps.LatLng(json.data[i].longitude, json.data[i].latitude));
+							map.setZoom(20);
+						}
+					}
 					loadAllMarkers(json.data);
 					clusterer = new MarkerClusterer(map, markers);
+					$("#loading").hide();
 				} else{
 					//display error message
 					alert(json.error.message);
@@ -35,7 +53,7 @@ function initialize() {
 
 
 
-function addMarker(location) { //@TODO zoek uit hoe die location in elkaar zit
+function addMarker(location) { 
 	var marker = new google.maps.Marker({
 		position: location,
 		map: map
@@ -67,7 +85,7 @@ function loadAllMarkers(monuments) {
 		})
 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                infowindow.setContent("<a href=\"/monument/" + monuments[i].id + "\">"+monuments[i].name + "<br/>" + monuments[i].street + ", " + monuments[i].streetNumber + "</a>");
+                infowindow.setContent("<a href=\"" + monumentURL.replace("0", monuments[i].id) + "\">"+monuments[i].name + "<br/>" + monuments[i].street + ", " + monuments[i].streetNumber + "</a>");
                 infowindow.open(map, marker);
             }
         })(marker, i));
